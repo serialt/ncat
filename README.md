ead16f8ea1067852ae644f59cc4dba7deb8a0740
serialt <tserialt@gmail.com>
2023-06-17 17:08:00 +0800

    fix

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-## cli 框架 sugar 版
-只使用标准库
+## ncat
+check tcp/icmp
 
 ```shell
-git clone -b sugar git@github.com:serialt/cli.git
+ncat -h
+
+Usage:
+
+    ncat [-p tcp/icmp] [host,port]
+
+Examples:
+
+    # ping google continuously
+    ncat www.google.com
+
+    # tcp check 
+    ncat -p tcp 8.8.8.8 22
+    ncat -p tcp github.com 443 80 22 5555
 ```
--- main.go
+++ main.go
@@ -3,49 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
 
-	"github.com/serialt/sugar/v3"
-	"golang.org/x/exp/slog"
+	probing "github.com/prometheus-community/pro-bing"
 )
 
 var usage = `
 Usage:
 
-    ping [-c count] [-i interval] [-t timeout] [--privileged] host
+    ncat [-p tcp/icmp] [host,port]
 
 Examples:
 
     # ping google continuously
-    ping www.google.com
+    ncat www.google.com
 
-    # ping google 5 times
-    ping -c 5 www.google.com
+    # tcp check 
+    ncat -p tcp github.com 22
+    ncat -p tcp github.com 443 80 22 5555
 
-    # ping google 5 times at 500ms intervals
-    ping -c 5 -i 500ms www.google.com
-
-    # ping google for 10 seconds
-    ping -t 10s www.google.com
-
-    # Send a privileged raw ICMP ping
-    sudo ping --privileged www.google.com
-
-    # Send ICMP messages with a 100-byte payload
-    ping -s 100 1.1.1.1
 `
 
 func init() {
 	flag.BoolVar(&appVersion, "v", false, "Display build and version messages")
-	flag.StringVar(&protocol, "p", "icmp", "[tcp,udp,icmp]")
+	flag.StringVar(&protocol, "p", "icmp", "[tcp,udp,icmp], default icmp")
+	flag.IntVar(&timeout, "T", 1000, "")
 	flag.BoolVar(&privileged, "privileged", false, "")
+
 	flag.Usage = func() {
 		fmt.Print(usage)
 	}
 	flag.Parse()
 
-	slog.SetDefault(sugar.New())
-
 }
+
+var (
+	// 版本信息
+	appVersion bool // 控制是否显示版本
+	APPVersion = "v0.0.2"
+	BuildTime  = "2006-01-02 15:04:05"
+	GitCommit  = "xxxxxxxxxxx"
+
+	protocol   string
+	timeout    int
+	privileged bool
+	wg         sync.WaitGroup
+)
 
 func main() {
 	if appVersion {
@@ -60,7 +67,88 @@ func main() {
 		return
 	}
 
-	host := flag.Arg(0)
-	Ping(host)
-	// service()
+	switch protocol {
+	case "icmp":
+		host := flag.Arg(0)
+		Ping(host)
+	case "tcp", "udp":
+		host := flag.Args()[0]
+		port := flag.Args()[1:]
+		var checkList []string
+		for _, v := range port {
+			member := fmt.Sprintf("%s:%s", host, v)
+			checkList = append(checkList, member)
+		}
+		for _, m := range checkList {
+			go func(_m string) {
+				wg.Add(1)
+				TCPOrUDPAlive(protocol, _m)
+			}(m)
+		}
+		fmt.Println(checkList)
+
+	}
+
+	wg.Wait()
+}
+
+func Ping(host string) {
+	// count := -1
+	// size := 24
+	// ttl := 64
+	pinger, err := probing.NewPinger(host)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+
+	// listen for ctrl-C signal
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			pinger.Stop()
+		}
+	}()
+
+	pinger.OnRecv = func(pkt *probing.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d ttl=%v time=%v\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.TTL, pkt.Rtt)
+	}
+	pinger.OnDuplicateRecv = func(pkt *probing.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d ttl=%v time=%v (DUP!)\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.TTL, pkt.Rtt)
+	}
+	pinger.OnFinish = func(stats *probing.Statistics) {
+		fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+		fmt.Printf("%d packets transmitted, %d packets received, %d duplicates, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketsRecvDuplicates, stats.PacketLoss)
+		fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+	}
+
+	pinger.Count = -1
+	pinger.Size = 56
+
+	pinger.SetPrivileged(privileged)
+
+	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+	err = pinger.Run()
+	if err != nil {
+		fmt.Println("Failed to ping target host:", err)
+	}
+}
+
+func TCPOrUDPAlive(protocol string, host string) {
+	tcpTimeOut := time.Duration(3000)
+	conn, err := net.DialTimeout(protocol, host, time.Duration(tcpTimeOut*time.Millisecond))
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		wg.Done()
+		return
+	} else {
+		fmt.Printf("Open %s: %s\n", protocol, host)
+	}
+	conn.Close()
+	wg.Done()
 }
